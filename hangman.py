22f9843e9cd22ff4834aafcf11c5f10ba041f33f
AnthonyChablov <86855033+AnthonyChablov@users.noreply.github.com>
2022-01-17 14:31:23 -0500

    created the board layout of hangman game

--- hangman.py
+++ hangman.py
@@ -1,1 +1,16 @@
 
+# A hang man game
+
+# The Components 
+
+# In hang-man, the player gets 6 chances to correctly guess the word.
+
+hangman_dict={0: '-------\n|\n|\n|\n|\n|\n------------', 
+1: '-------\n|     |\n|     O\n|\n|\n|\n------------',
+2:'-------\n|     |\n|     O\n|     |\n|\n|\n------------',
+3:'-------\n|     |\n|     O\n|   --|\n|\n|\n------------',
+4:'-------\n|     |\n|     O\n|   --|--\n|\n|\n------------',
+5:'-------\n|     |\n|     O\n|   --|--\n|    /\n|\n------------',
+6:'-------\n|     |\n|     O\n|   --|--\n|    / \ \n|\n------------'}
+
+
