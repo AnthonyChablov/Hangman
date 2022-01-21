5efc8cb731f5016d9b188e713df995dc7cbb360b
AnthonyChablov <86855033+AnthonyChablov@users.noreply.github.com>
2022-01-20 20:17:35 -0500

    fully functioning hangman game

--- hangman.py
+++ hangman.py
@@ -3,31 +3,23 @@
 #One player things of a word and the other tries to guess that word by suggesting letters and within a certain number of attempts
 
 #There are 6 attempts to hangman and for each incorrect attempt a limb will be added to the hang man post.
-#
-
 #Steps to complete!
 
+#   The Hangman Board
 
-#The Hangman Board
 
 def hangman(i):
-    hangman_dict={0: '-------\n|\n|\n|\n|\n|\n------------', 
-    1: '-------\n|     |\n|     O\n|\n|\n|\n------------',
-    2: '-------\n|     |\n|     O\n|     |\n|\n|\n------------',
+    hangman_dict={6: '-------\n|\n|\n|\n|\n|\n------------', 
+    5: '-------\n|     |\n|     O\n|\n|\n|\n------------',
+    4: '-------\n|     |\n|     O\n|     |\n|\n|\n------------',
     3: '-------\n|     |\n|     O\n|   --|\n|\n|\n------------',
-    4: '-------\n|     |\n|     O\n|   --|--\n|\n|\n------------',
-    5: '-------\n|     |\n|     O\n|   --|--\n|    /\n|\n------------',
-    6: '-------\n|     |\n|     O\n|   --|--\n|    / \ \n|\n------------'}
+    2: '-------\n|     |\n|     O\n|   --|--\n|\n|\n------------',
+    1: '-------\n|     |\n|     O\n|   --|--\n|    /\n|\n------------',
+    0: '-------\n|     |\n|     O\n|   --|--\n|    / \ \n|\n------------'}
     return hangman_dict[i]
 
-#Loop max turn count to 6.
-#   In hangman there are 6 chances to guess the word.
-tries = 0
-
 #   Get input of the mystery word.
-
-
-def get_word():
+def mystery_word():
     mysteryword=''
     while mysteryword.isalpha()==False:
         mysteryword=input('Player 1 please input the mystery word: ')
@@ -36,37 +28,112 @@ def get_word():
         elif mysteryword.isalpha()==True:
             return mysteryword.upper()
 
+
+#   Get input of the player guess.
 def player_guess():
     playerguess=''
     while playerguess.isalpha()==False:
         playerguess=input('Player 2 Please enter a letter or guess the word: ')
-        if playerguess.isalpha()==True:
+        if playerguess.isalpha()==False:
+            print('Player 1 please input a valid mystery word!')
+        elif playerguess.isalpha()==True:
             return playerguess.upper()
+0
+#   Game functionality.
+def game():
 
-
-
-
-
-
-print("Lets play hangman!!")
-mysteryword=get_word()
-
-def game(mysteryword):
-    word_completion= '' *len(mysteryword)
-    guessed=False
-    guessed_letters=[]
-    guessed_words=[]
+    play=True
+    
+    #   In hangman there are 6 chances to guess the word.
     tries=6
 
-    print(f'You have {tries} left')
-    print(word_completion)
-    print('\n')
-   
-    
-    while not guessed and tries >0:
-        playerguess=player_guess()
-        if len(playerguess)==1:
-            playerguess.append()
+    hiddenword=[]
+
+    guessedletters=[]
+    guessedwords=[]
+
+    mw=mystery_word()
+
+    for i in range(len(mw)):
+        hiddenword.append('_')
+
+    while play==True and tries>0: 
+        print(hiddenword)
+        pg=player_guess()
+
+        #   Check if the player guessed a letter.
+        if len(pg)==1:
+
+            #   Check if the player already guessed the letter.
+            if pg in guessedletters:
+                print('You have already guessed this letter, please try again. \n')
+
+            #   If the player guessed letter is WRONG (NOT in the mystery word.) 
+            elif pg not in mw:
+                tries -=1
+                print(f'Your guess of {pg} is wrong.')
+                guessedletters.append(pg)
+                #   Print out the hangman visual representation to signify a wrong guess. (Add a limb to the hangman.)
+                print(hangman(tries))
+                
+                #   If tries reaches 0
+                if tries == 0:
+                    print("Game Over, You have no more tires left.\n")
+                    print(f'The hidden word was {mw} \n')
+
+            #   The player guessed a correct letter that is in the mystery word.
+            elif pg in mw:
+                print(f'Your guess of {pg} is correct. ')
+                guessedletters.append(pg)
+
+                #   We are using list comprehension to 
+                index_pos=[i for i,letter in enumerate(mw) if letter==pg]
+                for index in index_pos:
+                    hiddenword[index]=pg
+                
+                #   If the player guesses all the letters inside the 
+                if ''.join(hiddenword) == mw:
+                    print("Congratulations, You have guessed the correct word! You win!\n")
+                    play==False 
+
+
+
+        #   Creating the conditions if the player guessed an entire word
+        elif len(pg) == len(mw):
+            if pg in guessedwords:
+                print('You have already guessed this word, please try again. \n')
+
+            #   If the player guessed word is WRONG. (NOT the mystery word.) 
+            elif pg != mw:
+                tries -=1
+                print(f'Your guess of {pg} is wrong. \n')
+                guessedwords.append(pg)
+                #   Print out the hangman visual representation. (Add a limb to the hangman.)
+                print(hangman(tries))
+
+                #   If tries reaches 0
+                if tries == 0:
+                    print("Game Over, You have no more tries left.\n")
+                    print(f'The hidden word was {mw} \n')
+
+            # The player guessed the correct word 
+            elif pg == mw:
+                print(f'Your guess of {pg} is correct. ')
+                guessedwords.append(pg)
+                play=False
+                print(f'Congratulations ! {mw} is the mystery word! You win!!\n')
+     
+        else:
+            print('Your guess is incorrect. Please try again. \n')
+            tries -=1
+            print(hangman(tries))
+
+
+
+game()
+
+
+
 
 
 
